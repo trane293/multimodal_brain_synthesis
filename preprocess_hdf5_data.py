fa0b20a72f996e5df6b1b395a06e729eaf4d7ea8
Anmol Sharma <anmol.sharma293@gmail.com>
2018-06-13 23:45:18 -0700

    edited preprocess_hdf5_data file to ensure filepaths are changed when run on computecanada

--- preprocess_hdf5_data.py
+++ preprocess_hdf5_data.py
@@ -15,9 +15,17 @@ elif 'flair.' in imagefile:
     i = 3
     seq_name = 'flair'
 '''
+import platform
 
-mount_path_prefix = '/home/anmol/'
-hdf5_filepath = mount_path_prefix + 'BRATS_Combined.h5'
+# to make the code portable even on cedar,you need to add conditions here
+node_name = platform.node()
+if node_name == 'XPS15':
+    # this is my laptop, so the cedar-rm directory is at a different place
+    mount_path_prefix = '/home/anmol/'
+    hdf5_filepath = mount_path_prefix + 'BRATS_Combined.h5'
+elif 'computecanada' in node_name: # we're in compute canada, maybe in an interactive node, or a scheduler node.
+    hdf5_filepath = '/scratch/asa224/asa224/Datasets/BRATS2018/HDF5_Datasets/' + 'BRATS_Combined.h5'
+
 hdf5_file = h5py.File(hdf5_filepath, 'r')
 hf = hdf5_file['combined']
 hgg_data = hf['training_data']
@@ -25,25 +33,25 @@ hgg_data = hf['training_data']
 t1 = hgg_data[:,0,...]
 t1 = np.swapaxes(t1, 3, 2)
 t1 = np.swapaxes(t1, 2, 1)
-np.save(open('T1.npz', 'wb'), t1)
+np.save(open('./BRATS/T1.npz', 'wb'), t1)
 del t1
 
 t2 = hgg_data[:,1,...]
 t2 = np.swapaxes(t2, 3, 2)
 t2 = np.swapaxes(t2, 2, 1)
-np.save(open('T2.npz', 'wb'), t2)
+np.save(open('./BRATS/T2.npz', 'wb'), t2)
 del t2
 
 t1ce = hgg_data[:,2,...]
 t1ce = np.swapaxes(t1ce, 3, 2)
 t1ce = np.swapaxes(t1ce, 2, 1)
-np.save(open('T1CE.npz', 'wb'), t1ce)
+np.save(open('./BRATS/T1CE.npz', 'wb'), t1ce)
 del t1ce
 
 t2flair = hgg_data[:,3,...]
 t2flair = np.swapaxes(t2flair, 3, 2)
 t2flair = np.swapaxes(t2flair, 2, 1)
-np.save(open('T2FLAIR.npz', 'wb'), t2flair)
+np.save(open('./BRATS/T2FLAIR.npz', 'wb'), t2flair)
 del t2flair
 
 print('hello')
