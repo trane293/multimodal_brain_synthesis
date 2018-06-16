da85cad46abf597583c0c654fbd816d1781efa01
Anmol Sharma <anmol.sharma293@gmail.com>
2018-06-16 13:58:44 -0700

    commented out test code

--- main_file.py
+++ main_file.py
@@ -27,9 +27,9 @@ if options.experiment == 0:
     input_modalities = ['T1', 'T2']
     output_weights = {'T2FLAIR': 1.0, 'concat': 1.0}
     exp = Experiment(input_modalities, output_weights, options.resultsdir, data, latent_dim=16, spatial_transformer=True)
-    exp.load_partial_model(folder=options.resultsdir + '/split0/', model_name='model', input_modalities=['T1', 'T2'], output_modality='T2FLAIR')
+    # exp.load_partial_model(folder=options.resultsdir + '/split0/', model_name='model', input_modalities=['T1', 'T2'], output_modality='T2FLAIR')
     # exp.load_model(options.resultsdir + '/split0/', model_name='model')
-    exp.run_test_minimal(data)
+    exp.run(data)
 else:
     print('Training model with 4 inputs and 4 outputs')
     data = Data('./BRATS', dataset='BRATS', trim_and_downsample=False, modalities_to_load=['T1', 'T2', 'T1CE', 'T2FLAIR'], normalize_volumes=False)
